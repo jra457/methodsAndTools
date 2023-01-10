960ffbb49baf02d250fc180d850771974d810681
jra457 <111715907+jra457@users.noreply.github.com>
2023-01-09 23:10:29 -0600

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -1,67 +1,60 @@
-import math
+from functions import openFile, numbers, dist, \
+    isPalindrome, divide, sq, greetUser, displayItem
+
 import pytest
 
-## opens a file in read mode
-## filename received as a parameter
-def openFile(filename):
-    infile = open(filename, "r")
+# ~~~~~ openFile ~~~~~
+def test_openFile():
 
-    print("File opened.")
-
-## takes two numbers and returns
-## the result of a division
-def numbers(num1, num2):
-    return num1 / num2
-
-## takes in two points
-## finds the distance between the points
-def dist(x1, y1, x2, y2):
-    dist = (x2 - x1) ** 2 + (y2 - y1) ** 2
-    dist = math.sqrt(dist)
-
-    return dist
-
-## takes in a string -- reverses it
-## then compares the two
-def isPalindrome(temp):
-    test = temp[::-1]
-
-    if(test == temp):
-        return True
-
-    else:
-        return False
-
-## has input to receive two numbers
-## divides the two, then outputs the result
-def divide():
-    num1 = int(input("Enter a number: "))
-    num2 = int(input("Enter another number: "))
-
-    div = num1 / num2
-
-    print("Your numbers divided is:", div)
-
-## returns the squareroot of a particular number
-def sq(num):
-    return math.sqrt(num)
+    assert openFile("testing.txt") == None
+# ~~~~~
 
 
+# ~~~~~ numbers ~~~~~
+def test_numbers():
 
+    assert numbers(64, 2) == 32
+# ~~~~~
+
+
+# ~~~~~ dist ~~~~~
+def test_dist():
+
+    assert dist(-4, -3, 4, 3) == 10
+# ~~~~~
+
+
+# ~~~~~ isPalindrome ~~~~~
+def test_isPalindrome():
+    input = [9, 1, 0, 1, 9]
+    assert isPalindrome(input) == True
+# ~~~~~
+
+
+# ~~~~~ divide ~~~~~
+def test_divide():
+
+    assert divide(128, 32) == 4
+# ~~~~~
+
+
+# ~~~~~ sq ~~~~~
 def test_sq():
+
     assert sq(4) == 2
+# ~~~~~
 
 
+# ~~~~~ greetUser ~~~~~
+def test_greetUser():
 
-## grabs user's name
-## greets them by their entire name
-## names should be strings that only accept letters! (A-Z, a-z)
-def greetUser(first, middle, last):
-    print("Hello!")
-    print("Welcome to the program", first, middle, last)
-    print("Glad to have you!")
+    assert greetUser('Linus', 'Benedict', 'Torvalds') == None
+# ~~~~~
 
-## takes in a Python list
-## attempts to display the item at the index provided
-def displayItem(numbers, index):
-    print("Your item at", index, "index is", numbers[index])
+
+# ~~~~~ displayItem ~~~~~
+def test_displayItem():
+    animals = ["Danger Noodle", "Disco Turkey", "Land Clouds", "Ocean Piglet", "Level 32: Raccoon", \
+        "Bumpy Desert Llama", "Panda Whale", "Danger Floof", "Trash Panda", "Danger Water Cow"]
+    assert displayItem(animals, 8) == None
+# ~~~~~
